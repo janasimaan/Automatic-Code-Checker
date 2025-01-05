14cbee5ae04880a76b8d53c572bfb9485cc3982f
jana simaan <104629175+janasimaan@users.noreply.github.com>
2025-01-05 21:18:17 +0200

    Add files via upload

---
+++ Models/CourseInfo.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace FinalProject.Models
+{
+    public class CourseInfo
+    {
+        public string courseName { get; set; }
+        public string courseYear { get;set; }
+        public List<Student> students { get;set; }
+        //convert from JSON to C# Class
+        public void Load_JSON() {
+            string dir = GlobalData.directory + GlobalData.Year + "\\" + GlobalData.Course
+                + "\\course_info.json";
+            string text = File.ReadAllText(dir);
+            var c = Newtonsoft.Json.JsonConvert.DeserializeObject<CourseInfo>(text);
+            if (c != null)
+            {
+                this.courseName = c.courseName;
+                this.courseYear = c.courseYear;
+                this.students = c.students;
+            }
+            else {
+                this.courseYear = GlobalData.Year;
+                this.courseName = GlobalData.Course;
+                this.students = new List<Student>();
+            }
+        }
+        //convert from C# class to JSON file
+        public void Save_JSON()
+        {
+            if (GlobalData.Year == "N" || GlobalData.Course == "N")
+            {
+                MessageBox.Show("please select a year and a course");
+                return;
+            }
+            else { 
+                var res = Newtonsoft.Json.JsonConvert.SerializeObject(this);
+                string target = GlobalData.directory + GlobalData.Year + "\\" + GlobalData.Course
+                    + "\\course_info.json";
+                System.IO.File.WriteAllText(target, res);
+            }
+        }
+    }
+
+
+    public class Student { 
+        public string Name { get; set; }
+        public int Id { get; set; }
+    }
+    public class StudentGrade { 
+        public Student student { get; set; }
+        public int grade { get; set; }
+    }
+}
---
+++ Models/GlobalData.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FinalProject
+{
+    public static class GlobalData
+    {
+        public static string directory = "C:\\FinalProject\\FinalProject\\FinalProject\\Years\\";
+        public static string Year = "N";
+        public static string Course = "N";      
+        public static string Homework = "N";
+    }
+}
